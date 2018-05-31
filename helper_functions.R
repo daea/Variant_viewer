5a97f7ae3d563a78c07358e3759ae1551dd747ee
Matt Cumming <matthew.cumming@gmail.com>
2018-05-31 12:29:07 -0400

    fonts formatted, agi verification, requests proteins seq based on successful variant requests, flow control for tree construction

--- helper_functions.R
+++ helper_functions.R
@@ -22,7 +22,7 @@ parseCommandLineAgis <- function() {
 
 getStatus <- function(responses, submission_ids) {
 	response_codes = lapply(responses, function(response) status_code(response))
-	names(response_codes) = submission_ids
+	names(response_codes) = unlist(lapply(responses, function(x) x$name))
 	for (name in names(response_codes)) {
 		if (response_codes[name] != 200) {
 			print(paste("The server could not return results for:", name, "."))
@@ -32,10 +32,13 @@ getStatus <- function(responses, submission_ids) {
 			print("An unknown error occurred.","AGI: ", name, ", Response Code: ", response_codes[name]) 
 		}
 	}
+	return(response_codes)
 }
 
 
-getVariants <- function (gids, base_url, right_url) {
+getVariants <- function (gids) {
+	base_url = "http://tools.1001genomes.org/api/v1.1/effects.json?type=snps;accs=all;gid="
+	right_url = ";effect=missense_variant"
 	variants = list()
 	for (i in 1:length(gids)) {
 		if (i == length(gids) && i %% 2 == 1) {
@@ -60,37 +63,7 @@ getVariants <- function (gids, base_url, right_url) {
 
 formatVariants <- function(variants) {
 	records = sum(unlist(lapply(variants, 
-								function(variant) length(content(variant)[["data"]]))))
-	
-	data = data.frame(matrix(nrow = records, ncol = 14)) # Create a dataframe of length = records
-	i = 1
-	for (agi in variants) {								# Iterate over list of response objects
-		for (record in content(agi)[["data"]]) {		# Get the content, iterate over records
-			data[i,] = rbind(unlist(record))	# Bind into a table and create dataset
-			i = i + 1
-		}
-	}
-	names(data) <- c("chr", 
-					 "pos",
-					 "strain",
-					 "type",
-					 "effect_impact",
-					 "functional_class",
-					 "codon_change",
-					 "amino_acid_change",
-					 "amino_acid_length",
-					 "gene_name",
-					 "transcript_biotype",
-					 "gene_coding",
-					 "transcript_id",
-					 "exon_rank"
-	)
-	return(data)
-}
-
-formatVariants2 <- function(variants) {
-	records = sum(unlist(lapply(variants, 
-								function(variant) length(content(variant)[["data"]]))))
+		function(variant) length(content(variant)[["data"]]))))
 	
 	data = data.frame(matrix(nrow = records, ncol = 15)) # Create a dataframe of length = records
 	i = 1
@@ -117,4 +90,48 @@ formatVariants2 <- function(variants) {
 					 "exon_rank"
 	)
 	return(data)
+}
+
+
+getProteinSeqs <- function(gids) {
+	proturl = "http://bar.utoronto.ca/webservices/bar_araport/get_protein_sequence_by_identifier.php?locus="
+	prot_responses = lapply(gids, function(x) GET(paste(proturl, x, sep="")))
+	
+	raw_seqs = unlist(lapply(prot_responses, function(response) content(response)$result[[1]]$sequence))
+	names(raw_seqs) = gids
+	return(raw_seqs)
+}
+
+
+getPfamDomains <- function(raw_seqs) {
+	pfamurl = "http://bar.utoronto.ca/eplant/cgi-bin/PfamAnnot.cgi"
+	prot_fam = lapply(raw_seqs, function(x) fromJSON(xml_text(content(POST(pfamurl, body = list(FASTAseq = x)))), simplifyMatrix = TRUE))
+	
+	prot_data = data.frame(matrix(ncol = 4, nrow = 0)) # Initialize an empty data frame
+	for (agi in names(prot_fam)) { 
+		for (domain in names(prot_fam[[agi]])) {
+			for (value in names(prot_fam[[agi]][[domain]])){
+				prot_data <- rbind(prot_data, rbind(c(agi, domain, value, prot_fam[[agi]][[domain]][[value]] )))
+			}
+		}
+	}
+	colnames(prot_data) <- c("agi", "pfam_domain", "value_type", "value")
+	pfam_domains <- prot_data %>% spread(value_type, value)
+	return(pfam_domains)
+}
+
+
+getCddDomains <- function(raw_seqs) {
+	cddurl  = "http://bar.utoronto.ca/eplant/cgi-bin/CDDannot.cgi"
+	cdd_res = lapply(raw_seqs, function(x) fromJSON(xml_text(content(POST(cddurl, body = list(FASTAseq = x)))), simplifyMatrix = TRUE))
+	cdd_data = data.frame(matrix(ncol = 3, nrow = 0))
+	for (agi in names(cdd_res)) {
+		for (domain in names(cdd_res[[agi]])) {
+			cdd_data <- rbind(cdd_data, rbind(c(agi, domain, cdd_res[[agi]][[domain]])))
+		}
+	}
+	colnames(cdd_data) <- c("agi", "cdd_domain", "residues")
+	cdd_residues <- cdd_data %>% separate_rows(residues) %>% extract(residues, into = c("residue", "location"), regex = "([A-Za-z])([0-9]+)")
+	cdd_residues$location <- as.numeric(cdd_residues$location)
+	return(cdd_residues)
 }
--- variant_viewer.R
+++ variant_viewer.R
@@ -1,6 +1,6 @@
 #### 2018 Matt Cumming - Provart Lab 2018  ####
 #### SNP Analyses ####
-{
+suppressMessages({
 library(dplyr)
 library(stringr)
 library("httr")
@@ -13,51 +13,36 @@ library(ggplot2)
 library(plotly)
 library(ggtree)
 source("helper_functions.R")
-}
+})
 
 #### Command line AGI ids ####
 gids = parseCommandLineAgis() 
 
 # Run the following two lines to use in RStudio
-args = "AT4G34000.1,AT1G45249.2"
-args = "AT4G34000.1,AT4G34000.2,AT4G34000.3, AT1G34000.5"
+args = "AT4G34000.1, AT1G45249.1,AT1G49720.1,AT3G19290.1"
 gids = unlist(str_extract_all(args, 'AT[0-9]G[0-9]+[.]?[0-9]?'))
 
-
 #### Get our variants from the Polymorph1001 API ####
-base_url = "http://tools.1001genomes.org/api/v1.1/effects.json?type=snps;accs=all;gid="
-right_url = ";effect=missense_variant"
-variants = getVariants(gids, base_url, right_url)
-getStatus(variants, gids)
+variants = getVariants(gids)
+variantStatus = getStatus(variants)
+success = names(variantStatus)[variantStatus == 200]
 data = formatVariants(variants)	# Parse JSON (slow)
 
 #### Calculate frequencies ####
-mutant = "p.([:alpha:]{3})([:digit:]+)([:alpha:]{3})" 	# Matches the snpEFF effect field and extracts variant info
-
-nsSNPs = summarise(group_by(data, gene_name, amino_acid_change, transcript_id),
-				   n = n()) 
-nsSNPs = mutate(nsSNPs, freq = n / 1135) 				# 1135 accessions in the sample total
-nsSNPs = mutate(group_by(nsSNPs, gene_name, amino_acid_change), # Split up that variant column
-		   original = str_match(amino_acid_change, mutant)[2],
-		   site     = as.numeric(str_match(amino_acid_change, mutant)[3]),
-		   variant  = str_match(amino_acid_change, mutant)[4],
-		   agi      = str_extract(transcript_id, 'AT[0-9]G[0-9]+')
-	)
-
+mutantPattern = "p.([:alpha:]{3})([:digit:]+)([:alpha:]{3})" 	# Matches the snpEFF effect field and extracts variant info
+nsSNPs = summarise(
+	group_by(data, gene_name, amino_acid_change, submission_id, pos),
+				   n = n()) %>% 
+		mutate(freq = n / 1135,         # Split up that variant column
+			original = str_match(amino_acid_change, mutantPattern)[2],
+			site     = as.numeric(str_match(amino_acid_change, mutantPattern)[3]),
+			variant  = str_match(amino_acid_change, mutantPattern)[4]
+		)
 nsSNPs$gene_name <- as.factor(nsSNPs$gene_name)
-nsSNPs$transcript_id <- as.factor(nsSNPs$transcript_id)
-
-gene_names <- nsSNPs %>% group_by(gene_name, transcript_id) %>% summarize()
-
-
-#### Request Protein Sequences ####
-proturl = "http://bar.utoronto.ca/webservices/bar_araport/get_protein_sequence_by_identifier.php?locus="
-prot_responses = lapply(gids, function(x) GET(paste(proturl, x, sep="")))
-raw_seqs = unlist(lapply(prot_responses, function(response) content(response)$result[[1]]$sequence))
-names(raw_seqs) = gids
-
+nsSNPs$submission_id <- as.factor(nsSNPs$submission_id)
 
 #### Alignment ####
+raw_seqs = getProteinSeqs(success)
 if (length(raw_seqs) == 1) {
 	aligned_seqs = AAStringSet(raw_seqs) # Don't align one sequence
 	tree = NULL
@@ -69,109 +54,109 @@ if (length(raw_seqs) == 1) {
 }
 
 #### Mapping Variants ####
-# Convert to a matrix
-matrix = data.frame(t(as.matrix(aligned_seqs)))
-consensus_length = c(1:nrow(matrix))
-matrix = gather(matrix)
-matrix$position = consensus_length
 
-# Data cleaning and organization
-temp = mutate(matrix, dummy = ifelse(value == "-",yes = 0, no = 1 ))
-temp2 = mutate(group_by(temp, key),
-			   value    = value, 
-			   position = position,
-			   map      = ifelse(dummy == 0, yes = NA, no = cumsum(dummy)),
-			   agi      = str_extract(key, 'AT[0-9]G[0-9]+')
-			)	
+alignmentMatrix = data.frame(t(as.matrix(aligned_seqs))) # Alignment to matrix
+consensus_length = c(1:nrow(alignmentMatrix))			# Create key value pairs		
+alignmentMatrix = gather(alignmentMatrix)							# Gather them into unique rows
+alignmentMatrix$position = consensus_length 				# Note consensus length is reused here
+alignmentMatrix = alignmentMatrix %>% 							
+	mutate(dummy = ifelse(value == "-",yes = 0, no = 1 )) %>%	# Convert gaps to numvers
+	group_by(key) %>%							# Group by key (agi)
+	mutate(value = value,
+		position = position,
+		map      = ifelse(dummy == 0, yes = NA, no = cumsum(dummy))
+	)											# Actual position to mapped position
+
 
 ## Map variants onto Alignment for Plotting
-final = left_join(x = temp2,
-		  y = nsSNPs,
-		  by = c("map" = "site", "agi" = "agi")
+variantMap = left_join(x = alignmentMatrix, y = nsSNPs,
+		  by = c("map" = "site", "key" = "submission_id")
 		  )
 
-frequency_data <- final %>% 
+frequency_data <- variantMap %>% 
 	mutate(freq  = ifelse(is.na(freq), 0, freq),# Turn NA's into 0's
 		   n     = ifelse(is.na(n), 0, n)) %>% 	# Group by position and agi 
-	group_by(position, agi) %>%
+	group_by(position, key) %>%
 	summarise(cum_freq = sum(freq),             # Calculate cumulative frequencies (diff from REF)
 			  intra = sum(freq > 0))	%>%	    
-	group_by(position)	%>%				      	# Group by position
-	summarise(av_freq = mean(cum_freq),         # Calculate average frequencies
+	group_by(position)	%>%				      	# Group by position, Calculate average frequencies
+	summarise(av_freq = mean(cum_freq),         
 			  interCount = sum(cum_freq > 0),
-			  intraCount = sum(intra)# Count the number of proteins with a variant
-	)
+			  intraCount = sum(intra)           
+	)											# Count the number of proteins with a variant
 
-#### Pfam Domains ####
-## Domains for Plot Annotations
-pfamurl = "http://bar.utoronto.ca/eplant/cgi-bin/PfamAnnot.cgi"
-prot_fam = lapply(raw_seqs, function(x) fromJSON(xml_text(content(POST(pfamurl, body = list(FASTAseq = x)))), simplifyMatrix = TRUE))
-
-prot_data = data.frame(matrix(ncol = 4, nrow = 0)) # Initialize an empty data frame
-for (agi in names(prot_fam)) { 
-	for (domain in names(prot_fam[[agi]])) {
-		for (value in names(prot_fam[[agi]][[domain]])){
-			prot_data <- rbind(prot_data, rbind(c(agi, domain, value, prot_fam[[agi]][[domain]][[value]] )))
-		}
-	}
-}
-
-colnames(prot_data) <- c("agi", "pfam_domain", "value_type", "value")
-pfam_domains <- prot_data %>% spread(value_type, value)
-
-
-#### CDD Domains ####
-
-cddurl  = "http://bar.utoronto.ca/eplant/cgi-bin/CDDannot.cgi"
-cdd_res = lapply(raw_seqs, function(x) fromJSON(xml_text(content(POST(cddurl, body = list(FASTAseq = x)))), simplifyMatrix = TRUE))
-cdd_data = data.frame(matrix(ncol = 3, nrow = 0))
-for (agi in names(cdd_res)) {
-	for (domain in names(cdd_res[[agi]])) {
-		cdd_data <- rbind(cdd_data, rbind(c(agi, domain, cdd_res[[agi]][[domain]])))
-	}
-}
-
-colnames(cdd_data) <- c("agi", "cdd_domain", "residues")
-cdd_residues <- cdd_data %>% separate_rows(residues) %>% extract(residues, into = c("residue", "location"), regex = "([A-Za-z])([0-9]+)")
-cdd_residues$location <- as.numeric(cdd_residues$location)
-
-
+#### Domain Annotations ####
+pfam_domains = getPfamDomains(raw_seqs)		# Pfam
+cdd_residues = getCddDomains(raw_seqs)		# Conserved Domain Database
 
 #### Combine CDD and pfam Data ####
-
 pfam_domains$startIndex <- as.numeric(as.character(pfam_domains$startIndex))
 pfam_domains$endIndex <- as.numeric(as.character(pfam_domains$endIndex))
 
-temp_start = left_join(pfam_domains, final, by = c("agi" = "key","startIndex" = "map"))
-temp_start$start_pos <- temp_start$position
-pfam_mapped = left_join(temp_start[c("agi", "pfam_domain", "endIndex", "start_pos","PfamAnnot", "Expect")], 
-						final, 
-						by = c(
-							"agi" = "key",
-							"endIndex" = "map")
-)
-pfam_mapped$end_pos <- pfam_mapped$position
-pfam_mapped <- subset(pfam_mapped, select = c("agi",
-											  "pfam_domain",
-											  "end_pos",
-											  "start_pos",
-											  "PfamAnnot",
-											  "Expect")
-)
-cdd_mapped <- left_join(cdd_residues, final, by = c("agi"= "key", "location" = "map"))
-cdd_mapped <- subset(cdd_mapped, select = c("agi",
-											"cdd_domain", 
-											"residue", 
-											"position" 
-))
-cdd_mapped$agi <- as.factor(cdd_mapped$agi)
-cdd_mapped$domain <- cdd_mapped$cdd_domain
-cdd_mapped <-cdd_mapped %>% 
-	mutate(start_pos = position, 
-		   end_pos = position + 1
-	) %>%
+temp_start = left_join(pfam_domains, variantMap, 
+					   by = c("agi" = "key","startIndex" = "map")) %>%
+	mutate(start_pos = position) %>% 
+	select(c("agi", "pfam_domain", "endIndex", "start_pos","PfamAnnot", "Expect"))
+
+pfam_mapped = left_join(temp_start, variantMap, by = c("agi" = "key", "endIndex" = "map")) %>%
+	mutate(end_pos = position) %>%
+	select(c("agi", "pfam_domain", "end_pos", "start_pos", "PfamAnnot", "Expect")) %>%
+	mutate(domain = pfam_domain)
+
+cdd_mapped <- left_join(cdd_residues, variantMap, by = c("agi"= "key", "location" = "map")) %>%
+	select(c("agi", "cdd_domain", "residue", "position")) %>% 
+	mutate(domain = cdd_domain) %>%
+	mutate(start_pos = position, end_pos = position + 1) %>%
 	select(-c(position))
-pfam_mapped$domain <- pfam_mapped$pfam_domain
+
+cdd_pfam <- full_join(pfam_mapped, cdd_mapped) %>% 
+	select(-c(pfam_domain, cdd_domain))
+
+if (!is.null(tree)) {
+	phylogeny <- ggtree(tree)
+	
+	# Pull the labels from the tree, so we can plot them in the right order
+	ids = data.frame(label = subset(phylogeny$data, isTip == TRUE)["label"],
+					 y =  subset(phylogeny$data, isTip == TRUE)["y"])		
+	# Convert tip labels to AGI
+	ids <- ids %>% 
+		group_by(label) %>%
+		mutate(agi = paste(str_split(label, pattern = "_")[[1]][2:3], collapse = ".")) %>%
+		arrange(y) 
+	
+	# Collect gene names from variant table
+	gene_names <- nsSNPs %>% group_by(gene_name, submission_id) %>% summarize()
+	
+	# If the gene name is null, plot the agi instead
+	ids = left_join(ids, gene_names, by = c("agi" = "submission_id"))
+	ids = mutate(ids, gene_name = ifelse(is.na(gene_name), 
+										 yes = as.character(agi), 
+										 no = as.character(gene_name)))
+	cdd_pfam <- left_join(cdd_pfam, ids, by = c("agi" = "agi"))
+	cdd_pfam$agi <- as.factor(cdd_pfam$agi)
+	
+	# In the order they are in the ids table or else they will plot in the wrong order
+	levels(cdd_pfam$gene_name) <- ids$gene_name
+	
+} else if (is.null(tree)) {
+	
+	ids <- nsSNPs %>% group_by(gene_name, submission_id) %>% summarize()
+	names(ids) <- c("name", "submission_id")
+	ids <- ids %>% 
+		group_by(name) %>%
+		mutate( gene_name = ifelse(is.na(name), 
+								   yes = as.character(submission_id), 
+								   no = as.character(name)))
+	
+	cdd_pfam <- left_join(cdd_pfam, gene_names, by = c("agi" = "agi"))
+	cdd_pfam$agi <- as.factor(cdd_pfam$agi)
+	
+	# In the order they are in the ids table or else they will plot in the wrong order
+	levels(cdd_pfam$gene_name) <- ids$gene_name
+}
+
+
+
 
 ####################### Working With Plotly ###########################
 #### Whisker plot ####
@@ -190,22 +175,30 @@ p1 <- plot_ly(frequency_data, x= ~position, y = ~av_freq,
 ) %>% 
 	layout(showlegend = FALSE,
 		   yaxis = list(
-		   	title = "frequency"),
+		   	title = "frequency",
+		   	titlefont = list(
+		   		font = "sans-serif",
+		   		size = 12
+		   	),
+		   	tickfont = list(
+		   		size = 10,
+		   		font = "sans-serif"
+		   	)),
 		   xaxis = list(
 		   	title = "Position in Multiple Sequence Alignment")
 	) %>%
-	add_trace(final, x =~final$position, y = ~final$freq,
+	add_trace(variantMap, x =~variantMap$position, y = ~variantMap$freq,
 			  name = "nsSNPs",
 			  type = "scatter",
 			  mode = "markers",
 			  marker = list(size = 5),
 			  text = paste(
-			  	paste("Gene Name:",final$gene_name),
-			  	paste("Reference: ", final$original),
-			  	paste("missense Variant: ", final$variant),
-			  	paste("Frequency: ", round(final$freq, 5)),
-			  	paste("Position:", final$map),
-			  	paste("# Accessions: ", final$n),
+			  	paste("Gene Name:",variantMap$gene_name),
+			  	paste("Reference: ", variantMap$original),
+			  	paste("missense Variant: ", variantMap$variant),
+			  	paste("Frequency: ", round(variantMap$freq, 5)),
+			  	paste("Position:", variantMap$map),
+			  	paste("# Accessions: ", variantMap$n),
 			  	sep = "<br>"
 			  ),
 			  
@@ -218,31 +211,19 @@ p2 = ggseqlogo(logo) + theme(
 							 panel.background = element_rect(fill = "grey97")
 							 ) +
 	theme(legend.title = element_blank(),
-		  axis.text.x = element_blank()) +
-	labs(y = "bits")
+		  axis.text.x = element_blank(),
+		  axis.text.y = element_text(size=10))
+p2 = ggplotly(p2) %>% 
+	layout(yaxis = list(
+			tickfont = list(
+				size = 10,
+				font = "sans-serif"),
+			titlefont = list(
+				size = 12,
+				font = "sans-serif"),
+			title = "bits")
+	)
 
-cdd_pfam <- full_join(pfam_mapped, cdd_mapped) %>% 
-	select(-c(pfam_domain, cdd_domain))
-
-
-phylogeny <- ggtree(tree)
-ids = data.frame(label = subset(phylogeny$data, isTip == TRUE)["label"],
-				 y =  subset(phylogeny$data, isTip == TRUE)["y"])
-ids <- ids %>% 
-	group_by(label) %>%
-	mutate(agi = paste(str_split(label, pattern = "_")[[1]][2:3], collapse = ".")) %>%
-	arrange(y)
-
-ids = left_join(ids, gene_names, by = c("agi" = "transcript_id"))
-
-
-ids = mutate(ids, gene_name = ifelse(is.na(gene_name), 
-				  yes = as.character(agi), 
-				  no = as.character(gene_name)))
-
-cdd_pfam <- left_join(cdd_pfam, ids, by = c("agi" = "agi"))
-cdd_pfam$agi <- as.factor(cdd_pfam$agi)
-levels(cdd_pfam$gene_name) <- ids$gene_name
 
 p3 <- ggplot(cdd_pfam, aes(y = y, x = end_pos, fill = domain, alpha = 0.5)) +
 	geom_rect(aes(y = y, x = end_pos,
@@ -255,6 +236,16 @@ p3 <- ggplot(cdd_pfam, aes(y = y, x = end_pos, fill = domain, alpha = 0.5)) +
 		labels = levels(cdd_pfam$gene_name)) + 
 	theme(legend.title = element_blank(),
 		  axis.title.y = element_blank())
+
+p3 <- ggplotly(p3) %>%
+	layout(yaxis = list(
+			tickfont = list(
+				size = 10,
+				font = "sans-serif"),
+			titlefont = list(
+				size = 12,
+				font = "sans-serif"))
+		)
 	
 right_plot <- subplot(p1, p3, p2, 
 		nrows = 3, 
@@ -268,3 +259,30 @@ right_plot <- subplot(p1, p3, p2,
 			 )
 htmlwidgets::saveWidget(right_plot, "variant_plot.html")
 
+######################################
+
+#plotting the msa underneath
+
+p3 <- ggplot(cdd_pfam, aes(y = y, x = end_pos, fill = domain, alpha = 0.5)) +
+	geom_rect(aes(y = y, x = end_pos,
+				  ymin = y - 0.45, 
+				  ymax = y + 0.45,
+				  xmin = start_pos - 0.5,
+				  xmax = end_pos - 0.5)) + 
+	scale_y_continuous(
+		breaks = seq(1:length(levels(cdd_pfam$gene_name))),
+		labels = levels(cdd_pfam$gene_name)) + 
+	theme(legend.title = element_blank(),
+		  axis.title.y = element_blank()) +
+	geom_text(aes(y = variantMap$key, x = variantMap$position, label = variantMap$value))
+
+p3 <- ggplotly(p3) %>%
+	layout(yaxis = list(
+		tickfont = list(
+			size = 10,
+			font = "sans-serif"),
+		titlefont = list(
+			size = 12,
+			font = "sans-serif"),
+		title = "bits")
+	)
