ac614bef45fb12059d7b37b8d389fddbae2a2e26
Matt Cumming <matthew.cumming@gmail.com>
2018-06-14 09:20:33 -0400

    Changed layout, tried select2 as an autocomplete

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,80 @@
-var data = [{ id: "ABF3", text: "AT4G34000.1"},
-			{ id: "ABF2", text: "AT1G45249.1"},
-			{ id: "ABF1", text: "AT1G49720.1"},
-			{ id: "ABF4", text: "AT3G19290.1"} 
-	];
+// Matthew Cumming - Provart Lab
+// Summer 2018
+// Sample Datasets for testing
 
+alert("main is loaded");
 
+var AGI_URL = "http://bar.utoronto.ca/eplant/cgi-bin/idautocomplete.cgi?species=Arabidopsis_thaliana&term=ABI3";
+var DEFAULT_AGIS = "http://localhost:8000/web-projects/variant-viewer/scripts/data.json";
 
-
+var agis;
 
 $(document).ready(function() {
-	$('.agiSelector').select2({
-		placeholder: "Start typing an AGI",
-		data: data
+	
+	$.get(DEFAULT_AGIS, function (data) {
+		$('.agiSelector').select2({
+			delay: 250,
+			placeholder: "Start typing an AGI",
+			minimumInputLength: 3,
+			data: formatOptions(data),
+			tags: true// default list to show 
+		});
 	});
 
-	$('#sendAgis').click( function() {
-		var agis = $('.agiSelector').select2('data');
-		var agiLength = agis.length;
-		for (var i =0;i<agis.length;i++) {
-			alert(agis[i].text);
-		}
+	// To get the selected values in the <select>
+	$('#sendAgis').click(function() {
+		var inputted = $('.agiSelector').select2('data');	
+		console.log(inputted);
+		var submission = formatSelected(inputted);
+		console.log(submission);
 	});
 
+
+	
 });
 
 
+function formatOptions(rawData) {
+	var formattedData = [];
+	for(var i = 0; i < rawData.length; i++) {
+		formattedData.push({"id": i + 1, "text": rawData[i]});
+	};
+	console.log(formattedData);
+	return formattedData;
+	
+}
 
+
+function formatSelected(selected) {
+	var submitArguments = [];
+	for(var i=0; i< selected.length; i++) {
+		var agi = selected[i].text.split("/")[0];
+		if (verifyAgi(agi) === true) {
+			submitArguments.push(selected[i].text.split("/")[0]);
+		} else if (verifyAgi === false) {
+			alert(agi + ": was not formatted correctly");
+		}
+	return submitArguments.join(',');
+	}
+}
+
+function verifyAgi(agiId) {
+	var re = /AT[0-9]G[0-9]+[.]?[0-9]?/;
+	var result = re.test(agiId);
+	return result;
+}
+
+/AT[0-9]G[0-9]+[.]?[0-9]?/
+/*
+(function ($) {
+	$.fn.refreshDataSelect2 = function (data) {
+		this.select2('data', data);
+		// Update options
+		var $select = $(this[1]);
+		var options = data.map(function(item) {
+			return '<option value="' + item.id + '">' + item.text + '</option>';
+		});
+        $select.html(options.join('')).change();
+	};
+})(jQuery);
+*/
