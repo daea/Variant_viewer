259eafd89d40e8150a7f438a0fc02dfaccbefe1e
Matt Cumming <mcumming@bar.utoronto.ca>
2018-06-15 09:56:25 -0400

    Add and remove genes functions and buttons

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,9 @@
 // Summer 2018
 // Sample Datasets for testing
 
-//alert("main is loaded");
 
 var AGI_URL = "http://bar.utoronto.ca/eplant/cgi-bin/idautocomplete.cgi?species=Arabidopsis_thaliana&term=";
-
-var mySource = ["At1G34000.1", "AT2G49720.1", "AT1G45249.1"];
+var queryAgis = [];
 
 // Document is loaded?
 window.addEventListener("load", function() {
@@ -17,8 +15,26 @@ window.addEventListener("load", function() {
 	// when a user lifts a key, trigger the hinter() function
 	agi_input.addEventListener("keyup", function(event) {hinter(event)});
 
+	// when a user clicks addGenes trigger the addGene function
+	var add_gene = document.getElementById('addAgi');
+	add_gene.addEventListener('click', function(event) {addGene(agi_input)});
+
 	// global XMLHttpRequest (json request) object
 	window.hinterXHR = new XMLHttpRequest();
+
+	var activeGenes = document.getElementById('addedGenes');
+	activeGenes.addEventListener("click", (event) => {
+		if (event.target.nodeName == 'A') {	
+			queryAgis = queryAgis.filter(function(e) { return e !== event.target.id });
+			document.getElementById(event.target.id).parentElement.remove();
+			if (queryAgis.length == 0) {
+			activeGenes.innerHTML = '<li class="list-group-item">You have not added any genes yet</li>';
+			};
+		} else {
+			console.log("Not finding the element");
+		}
+	});
+	
 });
 
 // the autocomplete part
@@ -28,9 +44,9 @@ function hinter(event) {
 	// this is the datalist element (options)
 	var huge_list = document.getElementById('huge_list');
 	// minimum number of characters typed into the field
-	var min_characters = 3;
+	var min_characters = 5;
 	// check to see how many characters are typed in the box
-	
+
 	if (input.value.length < min_characters) {
 		return;
 	} else {
@@ -42,11 +58,11 @@ function hinter(event) {
 		window.hinterXHR.onreadystatechange = function () {
 			// readystate = 0 unsent, 1 = opened, 2 = headers received, 3 = loading
 			// 4 means it's done, status 200 means success
-			if (this.readystate == 4 && this.status == 200) {
+			if (this.readyState == 4 && this.status == 200) {
 
 				// create a JSON response object
-				var response = JSON.pars( this.responseText );
-				
+				var response = JSON.parse( this.responseText );
+
 				// empty the datalist element of options
 				huge_list.innerHTML = "";
 
@@ -61,84 +77,67 @@ function hinter(event) {
 				});
 			}
 		};
+
 		window.hinterXHR.open("GET", AGI_URL + input.value, true);
+
 		window.hinterXHR.send();
 	}
 }
 
 
-					
+function validateForm() {
+	// Get the input element
+	var input = document.getElementById('agiInput');
+	// Get the datalist
+	var huge_list = document.getElementById('huge_list');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-$(document).ready(function() {
-	
-	var myAGIs = new Bloodhound ({
-		datumTokenizer: Bloodhound.tokenizers.obj.whitespace,
-		queryTokenizer: Bloodhound.tokenizers.whitespace,
-		local: mySource
-	});
-	console.log(myAGIs);	
-	
-	$('#AGItypeahead').typeahead({
-		hint: true,
-		minLength: 1, 
-		highlight: true
-	},
-	{
-		name: 'mySource',
-		source: myAGIs
-	});
-
-
-});
-
-
-function formatOptions(rawData) {
-	var formattedData = [];
-	for(var i = 0; i < rawData.length; i++) {
-		formattedData.push({"id": i + 1, "text": rawData[i]});
-	};
-	console.log(formattedData);
-	return formattedData;
-	
-}
-
-
-function formatSelected(selected) {
-	var submitArguments = [];
-	for(var i=0; i< selected.length; i++) {
-		var agi = selected[i].text.split("/")[0];
-		if (verifyAgi(agi) === true) {
-			submitArguments.push(selected[i].text.split("/")[0]);
-		} else if (verifyAgi === false) {
-			alert(agi + ": was not formatted correctly");
+	// If we find the input inside out list, we submit the form
+	for (var element of huge_list.children) {
+		if (element.value == input.value) {		
+			return true;
+			addGene(element.value);
 		}
-	return submitArguments.join(',');
+	alert("name input is invalid");
+	return false;
 	}
 }
 
-function verifyAgi(agiId) {
-	var re = /AT[0-9]G[0-9]+[.]?[0-9]?/;
-	var result = re.test(agiId);
-	return result;
+
+function addGene(input) {
+	var addedGenes = document.getElementById('addedGenes');	
+	if (queryAgis.length == 0) {
+		addedGenes.innerHTML = '';	
+		createListElement(input);
+		queryAgis.push(input.value);
+		addSubmitButton();
+	} else if (queryAgis.length == 10) {
+		alert("You have entered the maximum number of genes.");
+	} else if (queryAgis.includes(input.value) == true) {
+		alert("That gene is already included in the list.");
+	} else	{
+		createListElement(input);		
+		queryAgis.push(input.value);
+	}
+	console.log(queryAgis);	
 }
 
-// Regular expression for AGI's /AT[0-9]G[0-9]+[.]?[0-9]?/
-*/
+
+function createListElement(input) {
+	var Agi = document.createElement('li');
+	Agi.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center");
+	//Agi.classList.add("list-group-item-dark");
+	// add text to the list itm
+	Agi.appendChild(document.createTextNode(input.value));
+	Agi.innerHTML += "<a href='#' id='" + input.value + "' class='badge badge-pill badge-danger'>Remove</a>";
+	// add the item as an element to the addedGenes <ul>
+	addedGenes.appendChild(Agi);
+}
+
+function addSubmitButton() {
+	var input_panel = document.getElementById('inputPanel');
+	var submitAgis = document.createElement('button');
+	submitAgis.type = 'submit';
+	submitAgis.classList.add("btn", "btn-secondary");
+	submitAgis.innerHTML = 'Submit';
+	input_panel.appendChild(submitAgis);
+}
